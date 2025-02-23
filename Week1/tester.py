9a48165a2b89fad1a125a615164ba51bc32d1416
SSasindu <166973374+SSasindu@users.noreply.github.com>
2025-02-23 11:43:26 +0530

    fix testcase answer

--- Week1/tester.py
+++ Week1/tester.py
@@ -316,7 +316,7 @@ class LabTester:
         ]
         transformed = chain_transforms(point, transforms)
         self.assert_array_almost_equal(
-            transformed, [2.56055, -2.43085, -1.54043], decimal=3,
+            transformed, [8.04135, -2.03015, 7.18498], decimal=3,
             message="Complex transformations with nagative angles and additional transform"
         )
 
